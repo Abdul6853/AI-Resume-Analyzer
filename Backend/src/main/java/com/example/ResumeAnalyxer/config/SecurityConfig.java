b76eb4129e129c5585cf5fe4d226c8c172f26205
Abdul <abdulkalam974255@gmail.com>
2026-07-28 18:28:50 +0530

    added / to Security congig

--- Backend/src/main/java/com/example/ResumeAnalyxer/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/ResumeAnalyxer/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/user/register",
                                 "/user/login",
                                 "/v3/api-docs/**",
